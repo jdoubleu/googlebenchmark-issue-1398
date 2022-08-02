659b8521a70386a9ca2a4f04075f94cb38771745
jdoubleu <hi@jdoubleu.de>
2022-08-02 22:28:25 +0200

    trim down libtest

--- libtest.cpp
+++ libtest.cpp
@@ -1,39 +1,19 @@
-#include <cstring>
 #include <string>
-#include <sstream>
-#include <iomanip>
-#include <iostream>
 
-// taken from googletest which failed to compile
-// https://github.com/google/googletest/blob/main/googletest/src/gtest.cc#L4891
-
-std::string FormatByte(unsigned char value) {
-  std::stringstream ss;
-  ss << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
-     << static_cast<unsigned int>(value);
-  return ss.str();
+std::string GetHello()
+{
+    return std::string{"ello"};
 }
 
-std::string UrlEncode(const char* str) {
-  std::string result;
-  result.reserve(strlen(str) + 1);
-  for (char ch = *str; ch != '\0'; ch = *++str) {
-    switch (ch) {
-      case '%':
-      case '=':
-      case '&':
-      case '\n':
-        result.append("%" + FormatByte(static_cast<unsigned char>(ch)));
-        break;
-      default:
-        result.push_back(ch);
-        break;
-    }
-  }
-  return result;
+std::string SomeFunc()
+{
+    std::string msg{};
+    msg.append("H" + GetHello());
+
+    return msg;
 }
 
 int main()
 {
-    std::cout << UrlEncode("?param=value") << std::endl;
+    // do nothing
 }
